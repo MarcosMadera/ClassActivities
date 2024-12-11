76203e603676dbbba4c47157f7deb604661517c5
MarcosMadera <CodingRules66@gmail.com>
2024-12-10 21:46:01 -0600

    4.2homework

---
+++ Week4/4.2/4.2.1-SelectingDOMElements.js
@@ -0,0 +1,10 @@
+// select all unordered list (ul) elements
+
+// select all elements with the class "class-week"
+let getclass_week = document.getElementsByClassName("class-week");
+console.log(getclass_week);
+// select all elements with the class "nav-bar"
+let navQuery = document.querySelectorAll(".nav-bar");
+console.log(navQuery);/ select the element with the id "dog-picture" and save to a variable
+let dogQuery = document.querySelector("#dog-picture");
+console.log(dogQuery);
---
+++ Week4/4.2/4.2.2-NodeRelationships.js
@@ -0,0 +1,13 @@
+// select the root node
+const root = document.querySelector("html");
+// select the last child of the root node
+const lastRootChild = root.lastElementChild;
+console.log(lastRootChild);
+
+// select all the children of the body element
+const allChildrenBody = document.querySelector("body").children;
+console.log(allChildrenBody);
+// select the next sibling of the h2 node
+const h2Sibling = document.querySelector("h2").nextElementSibling;
+// select the parent element of the h1 node
+console.log(document);
